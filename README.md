6c90456de64a4b2e3f4d531cdd3ee30347274755
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-09-19 18:31:37 -0400

    found the raw files!

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 Add or remove icons from files or folders on any operating system.
 
-Fork this repository and modify `install.sh` to work with your projects!
+Fork this repository and modify it to work with your projects!
 
 ### License Notes
 Should be compatible with all operating systems (Windows version has to be 10 or above)
-- Created on July 2010 by Hank McShane
-- Implemented on Feb 2019 by c0d3d.net
+- Created on July 2010 by Hank McShane (http://www.hamsoftengineering.com/codeSharing/SetFileIcon/SetFileIcon.html)
+- Implemented on Feb 2019 by c0d3d.net (available @ Github.com)
 - Modified in 2019 by ForgivenNin
 - `needpix.png` has been labeled/licensed for commercial reuse & was made by `needpix.com`
--- install.sh
+++ install.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
-chmod +x
+chmod +x install.sh
 
-sudo cd ~/Downloads/
-# Assuming that when the user downloads the file, it will be here.
-echo "Installing 'set-icon' to /usr/local/bin/"
-sudo curl -LJO https://github.com/c0d3d-net/set-icon.macOS/raw/master/data.install
-sudo unzip -o  -C -qq ~/Downloads/data.install -d /usr/local/bin/
-# Modify to where `data.install` is recognized along with this file
-sudo chmod +x /usr/local/bin/set-icon
-echo "Done! Cleaning up temporary data packages."
-sudo rm ~/Downloads/data.install
-echo "Setup finished! Running commands!"
-sudo -image ~/set-icon_cross-platform/test/needpix.png
-sudo -file ~/set-icon_cross-platform/test/index.html
-# Sub out these file paths to your liking.
+os=${OSTYPE//[0-9.-]*/}
+case "$os" in
 
-sudo rm install.sh
+  darwin) # Apple/Mac
+    sudo cd ~/Downloads/
+    # Assuming that when the user downloads the file, it will be here.
+    echo "Installing 'set-icon' to /usr/local/bin/"
+    sudo curl -LJO https://github.com/c0d3d-net/set-icon.macOS/raw/master/data.install
+    sudo unzip -o  -C -qq ~/Downloads/data.install -d /usr/local/bin/
+    # Modify to where `data.install` is recognized along with this file
+    sudo chmod +x /usr/local/bin/set-icon
+    echo "Done! Cleaning up temporary data packages."
+    sudo rm ~/Downloads/data.install
+    echo "Setup finished! Running commands!"
+    sudo -image ~/set-icon_cross-platform/test/needpix.png
+    sudo -file ~/set-icon_cross-platform/test/index.html
+    # Sub out these file paths to your liking.
+  ;;
+
+  msys) # Windows 10+
+    ./dev/switch.cmd
+    # https://www.computerhope.com/unix/bash/exec.htm
+  ;;
+
+  *) # Linux (for now) and "*" = All other operating systems
+    # Response
+    echo "Uncompatible Operating system $OSTYPE"
+  exit 1
+
+esac
+
+# sudo rm dev/switch.cmd
+# sudo rm install.sh
