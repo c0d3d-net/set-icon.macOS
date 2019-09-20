57bdf6267496727dda4b0892beb04e7bd2c23cbd
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-09-19 22:57:34 -0400

    remove sudo + fix file paths

--- install.sh
+++ install.sh
@@ -7,20 +7,18 @@ case "$os" in
   darwin) # Apple/Mac
     sudo cd ~/Downloads/
     # Assuming that when the user downloads the file, it will be here.
-    echo "Installing 'set-icon' to /usr/local/bin/"
-    sudo curl -LJO https://github.com/c0d3d-net/set-icon.macOS/raw/master/data.install
-    sudo unzip -o  -C -qq ~/Downloads/data.install -d /usr/local/bin/
+    curl -LJO https://github.com/c0d3d-net/set-icon.macOS/raw/master/data.install
+    unzip -o -C -qq ~/Downloads/data.install -d /usr/local/bin/
     # Modify to where `data.install` is recognized along with this file
-    sudo chmod +x /usr/local/bin/set-icon
-    echo "Done! Cleaning up temporary data packages."
-    sudo rm ~/Downloads/data.install
+    chmod +x /usr/local/bin/set-icon
+    rm ~/Downloads/data.install
     echo "Setup finished! Running commands!"
-    sudo -image ~/set-icon_cross-platform/test/needpix.png
-    sudo -file ~/set-icon_cross-platform/test/index.html
+    -image ~/set-icon_cross-platform-master/test/needpix.png
+    -file ~/set-icon_cross-platform-master/test/index.html
     # Sub out these file paths to your liking.
   ;;
 
-  msys) # Windows 10+
+  msys) # Windows
     ./dev/switch.cmd
     # https://www.computerhope.com/unix/bash/exec.htm
   ;;
@@ -32,5 +30,5 @@ case "$os" in
 
 esac
 
-# sudo rm dev/switch.cmd
-# sudo rm install.sh
+# rm dev/switch.cmd
+# rm install.sh
