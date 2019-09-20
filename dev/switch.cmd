1d601f16378b435641ff7a326bd79aaea86dc438
fn <46826941+ForgivenNin@users.noreply.github.com>
2019-09-19 23:02:08 -0400

    Update switch.cmd

--- dev/switch.cmd
+++ dev/switch.cmd
@@ -1,3 +1,4 @@
+@ECHO OFF
 REM https://www.raymond.cc/blog/how-to-change-the-standard-folder-icon-to-another-icon/
 if [%1] == [] goto:eof
 ECHO [.ShellClassInfo] >%1\desktop.in
@@ -6,4 +7,4 @@ REM ^ENTER DESIRED FILE PATH ABOVE^
 move %1\desktop.in %1\desktop.ini
 attrib +S +H %1\desktop.ini
 attrib +R %1
-EXIT
+DEL "%~f0"
